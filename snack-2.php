69feed842a4243321d7caf8fb38b8caa35fe8a93
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-01-18 14:34:58 +0100

    completed snack2

--- snack-2.php
+++ snack-2.php
@@ -1,3 +1,23 @@
 <?php
 /*Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”*/
+
+//?name=andrea&mail=andrea@hotmail.it&age=30
+
+if(isset($_GET["name"]) && isset($_GET["mail"]) && isset($_GET["age"])){
+
+
+    if(strlen($_GET["name"]) > 3 && strpos($_GET["mail"], "@") != null && strpos($_GET["mail"], ".") != null && intval($_GET["age"]) == true) {
+        echo "Accesso riuscito";
+    }
+    else{
+        echo"Accesso negato";
+    }
+}
+else{
+    echo"Accesso negato";
+}
+
+
 ?>
+
+
