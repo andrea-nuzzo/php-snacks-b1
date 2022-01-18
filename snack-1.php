200b1f5f06f4bbf60f921e845463b41d90003120
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-01-18 14:03:24 +0100

    completed snack1

--- snack-1.php
+++ snack-1.php
@@ -1,4 +1,33 @@
 <?php
 /*Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60*/
+
+
+$games = [
+    [
+        "home_team" => "Dinamo Sassari",
+        "score_home-team" => 88,
+        "outside_team" => "Trento",
+        "score_outside_team" => 80
+    ],
+    [
+        "home_team" => "VL Pesaro",
+        "score_home-team" => 85,
+        "outside_team" => "Olimpia Milano",
+        "score_outside_team" => 82
+    ],
+    [
+        "home_team" => "Trieste",
+        "score_home-team" => 83,
+        "outside_team" => "Reggiana",
+        "score_outside_team" => 85
+    ],
+    
+];
+
+for($i = 0; $i< count($games); $i++){
+    
+    echo"{$games[$i]['home_team']} - {$games[$i]['outside_team']} | {$games[$i]['score_home-team']}-{$games[$i]['score_outside_team']} <br>";
+}
+
 ?>
